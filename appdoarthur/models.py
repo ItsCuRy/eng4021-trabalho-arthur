be422d2518d8a6b002eb936d8371cb09e72d2577
ItsCuRy <129109715+ItsCuRy@users.noreply.github.com>
2023-05-12 23:46:42 -0300

    Update models.py

--- appdoarthur/models.py
+++ appdoarthur/models.py
@@ -1,12 +1,12 @@
-from django.db import models
+from django.db import models #Importa o módulo models do pacote django.db
 
-class Task(models.Model):
+class Task(models.Model): #Define o modelo de uma Task com título, descrição, data de entrega, e uma checkbox de conclusão
   title = models.CharField(max_length=50)
   description = models.TextField()
   due_date = models.DateField()
   done = models.BooleanField()
 
-class Champ(models.Model):
+class Champ(models.Model): #Define o modelo de um Champ com nome do campeão, função, posição e tipo
   campeão = models.CharField(max_length=50)
   função = models.TextField()
   posição = models.CharField(max_length=50)
