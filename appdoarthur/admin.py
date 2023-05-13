ee499651b1ca8280b4f6191a3dfc8247d51476ef
ItsCuRy <129109715+ItsCuRy@users.noreply.github.com>
2023-05-12 23:40:04 -0300

    Update admin.py

--- appdoarthur/admin.py
+++ appdoarthur/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from .models import Task
-from .models import Champ
+from django.contrib import admin #Importa o módulo admin do pacote django.contrib 
+from .models import Task #Importa a função Task do arquivo .models 
+from .models import Champ #Importa a função Champ do arquivo .models
 
-admin.site.register(Task)
-admin.site.register(Champ)
+admin.site.register(Task) #Registra o modelo Task para gerenciamento dos registros no admin do site
+admin.site.register(Champ) #Registra o modelo Champ para gerenciamento dos reistros no admin do site
